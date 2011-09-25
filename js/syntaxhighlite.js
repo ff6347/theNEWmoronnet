c68ab4b1341fd85e0f7a1e59593c06e04fae7a63
fabiantheblind <fabiantheblind@googlemail.com>
2011-09-25 19:37:06 +0200

    lots of styling

---
+++ js/syntaxhighlite.js
@@ -0,0 +1,5 @@
+// This function is executed when the page has finished loading.
+jQuery(function($) {
+    // This function highlights (by default) pre and code tags which are annotated correctly.
+    $.syntax(    inlineSelector: "code.syntax");
+});
